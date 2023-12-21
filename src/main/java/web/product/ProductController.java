fbb65419e3756c4fa1aa8c4145df331f3c485ea2
Anatoly Ostrovsky <anatolyostrovsky@gmail.com>
2023-12-21 21:23:46 +0200

    Working table example

--- src/main/java/web/product/ProductController.java
+++ src/main/java/web/product/ProductController.java
@@ -7,6 +7,8 @@ import app.db.Db;
 import app.models.Product;
 import io.javalin.http.Context;
 import j2html.tags.DomContent;
+import java.util.Arrays;
+import java.util.List;
 import org.jetbrains.annotations.NotNull;
 
 public class ProductController {
@@ -16,13 +18,36 @@ public class ProductController {
   public static void delete(@NotNull Context ctx, @NotNull String s) {}
 
   public static Context getAll(@NotNull Context ctx) {
+    List<Product> items = Db.queryList(Product.class, "SELECT * FROM products");
+    String[] fields =
+        Arrays.stream(Product.class.getDeclaredFields())
+            .map(field -> field.getName())
+            .toArray(String[]::new);
 
     return render(
         ctx,
-        ul(
-            Db.queryList(Product.class, "SELECT * FROM products").stream()
-                .map(p -> li(p.title))
-                .toArray(DomContent[]::new)));
+        table(
+            thead(Arrays.stream(fields).map(field -> th(field)).toArray(DomContent[]::new)),
+            tbody(
+                items.stream()
+                    .map(
+                        item ->
+                            tr(
+                                Arrays.stream(fields)
+                                    .map(
+                                        f -> {
+                                          try {
+                                            return td(
+                                                String.valueOf(
+                                                    Product.class.getDeclaredField(f).get(item)));
+                                          } catch (IllegalAccessException e) {
+                                            throw new RuntimeException(e);
+                                          } catch (NoSuchFieldException e) {
+                                            throw new RuntimeException(e);
+                                          }
+                                        })
+                                    .toArray(DomContent[]::new)))
+                    .toArray(DomContent[]::new))));
   }
 
   public static void getOne(@NotNull Context ctx, @NotNull String s) {}
